42ea9990c3fe8091e84b68999272402e40d33b6f
JuniorFixHow <robertjuniorannan@gmail.com>
2023-01-24 00:45:38 +0000

    changed frequency

--- controller/requests.js
+++ controller/requests.js
@@ -17,8 +17,9 @@ export const postRequest = async(req, res)=>{
             temperature:0,
             max_tokens:3000,
             top_p:1,
-            frequency_penalty:0.5,
-            presence_penalty:0
+            frequency_penalty:0.0,
+            presence_penalty:0.0,
+            stop:["\n"]
         })
 
         res.status(200).send({
